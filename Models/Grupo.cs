6b3c9e0f8ca97503c9dab42e5d644d81a792fc6d
daniela6-hue <dalejandralopez6@gmail.com>
2026-09-09 15:22:09 -0600

    Agrege la carpeta models y el modelo grupo e insatalación de SQLite

---
+++ Models/Grupo.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsisGru.Models
+{
+    public class Grupo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Nombre { get; set; } = string.Empty;
+
+        public string? Descripcion { get; set; }
+        public string Responsable { get; set; } = string.Empty;
+        public string TelefonoResponsable { get; set; } = string.Empty;
+        public string? Organizacion { get; set; }
+    }
+}
