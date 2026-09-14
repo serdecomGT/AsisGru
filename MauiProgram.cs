02521880c53ce9ea63b463675df83fca9c15a966
daniela6-hue <dalejandralopez6@gmail.com>
2026-09-14 14:59:19 -0600

    Se creo el grupo de Pages

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using AsisGru.Data;
+using AsisGru.Services;
+using Microsoft.Extensions.Logging;
 
 namespace AsisGru
 {
@@ -7,18 +9,23 @@ namespace AsisGru
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
+
             builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+                .UseMauiApp<App>();
 
             builder.Services.AddMauiBlazorWebView();
 
+            builder.Services.AddSingleton<DatabaseService>();
+            builder.Services.AddSingleton<GrupoService>();
+            builder.Services.AddSingleton<ParticipanteService>();
+            builder.Services.AddSingleton<CategoriaService>();
+            builder.Services.AddSingleton<ActividadService>();
+            builder.Services.AddSingleton<AsistenciaService>();
+            builder.Services.AddSingleton<ReporteService>();
+            builder.Services.AddScoped<ThemeService>();
+
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
 
             return builder.Build();
